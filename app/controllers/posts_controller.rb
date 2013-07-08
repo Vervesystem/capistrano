40dbe8d1648ffddffda0a3f74124a7b5a6fde522
surya <surya@vervesys.com>
2013-07-08 15:37:03 +0530

    modified the post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-# verve System
+# verve System Pvt Ltd
 class PostsController < ApplicationController
   http_basic_authenticate_with :name => "admin", :password => "admin", :except => [:index, :show]
   # GET /posts
